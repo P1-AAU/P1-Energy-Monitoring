439ab1a56949097810f3d2db75c6cfee517c6255
Dressi123 <andreas.jack.2002@gmail.com>
2022-11-23 10:15:01 +0100

    added json-c library

---
+++ library/json-c-build/doc/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/andreasjackchristiansen/Desktop/UNI/P1/P1-Energy-Monitoring/library/json-c/doc
+# Build directory: /Users/andreasjackchristiansen/Desktop/UNI/P1/P1-Energy-Monitoring/library/json-c-build/doc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
