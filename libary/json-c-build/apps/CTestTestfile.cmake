7666e89f59b100067a7e205adf35902ee9ff68a7
Dressi123 <andreas.jack.2002@gmail.com>
2022-11-23 15:28:22 +0100

    fix

---
+++ libary/json-c-build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/andreasjackchristiansen/Desktop/UNI/P1/P1-Energy-Monitoring/libary/json-c/apps
+# Build directory: /Users/andreasjackchristiansen/Desktop/UNI/P1/P1-Energy-Monitoring/libary/json-c-build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
