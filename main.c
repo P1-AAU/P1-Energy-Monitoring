58d164de3277881051e535995a444fec86a78f30
Christian Lundholm <58586561+lundholm73@users.noreply.github.com>
2022-12-02 08:45:40 +0000

    Chrsitians shit

--- main.c
+++ main.c
@@ -9,29 +9,15 @@
 
 #define REFRESH_TOKEN "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlblR5cGUiOiJDdXN0b21lckFQSV9SZWZyZXNoIiwidG9rZW5pZCI6IjY0OWUyZjUyLTgwM2MtNDNjNC05MTlmLTg4NTY5OTczZTZjYSIsIndlYkFwcCI6WyJDdXN0b21lckFwaSIsIkN1c3RvbWVyQXBwQXBpIl0sInZlcnNpb24iOiIyIiwiaWRlbnRpdHlUb2tlbiI6IitkM05PeUUwMFE5YXowZ0sxd3VWVlRiTFJBaWF2eTh0eG1mSFhmWjYzN3d5L3RGVGpOTzFHTWtiNFB5dXZDNUVIQU4rWDgzbHh6TTdCK2R2ajlLVEYrVjA3eGc1U1NXcy9NdnVXQlRncjdwa2dKWjZZbFZyeU5aTjV4UTI2TFZsb0s2a2ppUUtWMlZmcTIzK1ZKaGQyYmJCdUo3b04wMmkzdzkwbVJ3TlhQcjRyc3d5U3RQZEo0TTVyajdoYmZRNDNvMngxY3hLeHRrSDNYWUs4TmxJSzR2MlYyekMzV2xwQ0xCMktKRzl1VDdCK1RKQzErODB1UzVjV1RlV0NrU3JzNTZMem1PVERaZnhKNzhGdVZwM2xvYW91bkFjWVNhcWswb2hPV05yeWhtOTRWSlcybDNFOWJ0RzBZckNVTkk3czl1azQ0cXIwUSt2QTFQcEdzRzVjaThPOS9YSGFTcVpzaWY5OXVFMHR4ZXVpZk8yb1ZBa0FmcHJXVUVkSVRDWHduMUtvTUxwMHVoQXU4TnN5Y3hWOGJHYnhxQmJIaS9NektNczJ2c2pUTW1xL2tPeG9VTkhKaUM1RklIUFhFc2VjckZMWWVPOUFVSnlRU01hZVIwTmtCRnVSKzdEaTE5SXdHME10YVVEdUU0TmE0aVR6Z3pSaVloUU51MFhyby91cGFua0lEV0VsUEhnWFRtWWZ1VHJsM0FFcHZIYmptQitVTkNXdi9CS2gzTTRrQ25nd1AvZlpNQmpubUVGdUwvU1d0ODM5V0NEVllZNlByWE9zZGk1bUs2TXRjZnlSUmtQU25oaTk1S3creVgrWWd1RjVManFMQ05tVHNqR2trQ2lrUFpQUVV3WXRGQkhFNjZCWHNFMDg0K0VWdXBMUnc5YUNiN0F2YVFkL3dZbnRFZzhocytXVmVHOGlZK1pzSFpaVFZvdU5OeEZYZjgrUHFrYnlKQjVXbGpwR1NFL3I1cDYrdzdWZkxObmNJMUx4N0VMYWpsb1pyL1gvYi83R3BPWWYvZlZTR2xXUW1CUlVRbmYrWUpnRG5RK0pvaFZJNzhDTk0xOVQxSFhJNnhvWlBlVTczTDBGMk1zeGFGYXMxRTNWVEtYZ1NwMERzbE80bUJZMHdiK0ZNeEFwSFFrUjhCVm1IQ1MrSnFzZ0YxNktJVmFZNDNva1NBZUZpaTdicnZNeGVlL1V4aCtlbTNzQzhabmozS2lRRzBFSTBsY3FJK0t0RkdkRXV1SDlOcEUwMzBuQXZUMTUyWDVYSGxIUEJoNlZEMFdvVmc3d2VyZzIySmt1QmdOOVF1MkVBVkJNeGE4aTE1WkFDc2l1UEtOY3U5ekU0UzlibDRuVmYzc3BWZHdlYmhpQVZweUJKdWxjMXJzR1lIMTB3ZTU1QWFJemN1MnVoU0kvcjJFbWV0NDJqZEk0MTAvVkxjbWNPK2dlNzFZdXk1R2lIUUF5Z0dWVkFRUHpEdFcyQnNLYThJSWhPVGdNTWRWNjl0MVZHbWZjR252TWNSMGdUb1JESnVra1B0b25iaUhTMWk5N0JmRU1zcTdlSGIzeklGQmg5QmwzMDZiL1NsWE41cE51Uk1sZ3duRDNaa2VBUzBJc1VwQkxqdUJJdkQ0Vm9sSEUzdlh0MVVSSnF0REtOV2pKWlhDQXNsSUtzTnJCZkFQSFZac2hTcGVPL0Qxa2c1R1RYY1R3QWlHNUhnNjNYeUR2S1Q2ajZ1ZzM5VGEwMy9MSVRpSEVrdHlxUkpoZmM1RmR3aHZhK1N3cDdzME0ya242L0MvWXdRa3Vub1grN1dPcnhTeTB5MFdZRHlGelNuZTlDTnpkbEZkMU5EVFB3Z1FNQ2NkUlIyZ1h1dk50OXBtZDljSlFCOXBhVUQyYy9sT25helZoNU5oNmZJZWdxYy9DKzFudGl1Um1EUGk4eTZpaEcxcjU1UDRVN1ZadXFnSUZ1RnNObjNieExyblc1cFVVM0ZPSnc2ZDY1U0FiVVpNdTRsZVBoMXVaMnFkVDV4MXhVNUVjTDJVRzFLRGtPZzRSOGFXNkxSc1hrNzZ0VVNLTzlrYk1Nc2ZLeklSNHRpd2s4azI4bU51YlQ3UTB4ZFd5bjZLOGZXY3d0Q1RRMzkyWnhGZ3dsVWdCUTg1SlRJMjdSd0drSHhDYTdZUGFkbHB2bXp6M2VBNVZGRzF3WERSQ2xEYWRtb0ZQWU5YaHNxNVlVcVFCMzU2RTFyUFB3c2JWRGQrQlhCV24vTy9ONnVPa1JNa0J1b2srb0hqenBWZml1U0s3d0YwWWN3WVF4OGxva3p2Zm1TNGZ6d0UvdDBXY3k3czh5Y2ZUakJCcTdPVjQzaWo5VWg4KzV1empyaG9CbVl5K2pxckhJZ2FGYlU5aFNpUjRBK2dWclpWMEluVncwL1MvR1FqOGk3R3d5K1hNQlFtSGNOMTZtWUJzUzRvTGcwUEVWdDFTUmNlWDVpTG5qSG9IemlkOGJ1WWtta1dvRllWbnN0RjREZEtPNjdWdk14T1dLZGRmYjMrNERqanRPVUF1M1BhVEY5ejdTTm1TWUZzSERENiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiUElEOjkyMDgtMjAwMi0yLTg4NzkyNTE5MDcwMSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2dpdmVubmFtZSI6IkFuZHJlYXMgSmFjayBDaHJpc3RpYW5zZW4iLCJsb2dpblR5cGUiOiJLZXlDYXJkIiwicGlkIjoiOTIwOC0yMDAyLTItODg3OTI1MTkwNzAxIiwiYjNmIjoiS2JXaWZZdjJrNDJTcytka3F1bktaNXhsVkFQQjNPa0NtVHNzdzFXVkNlYz0iLCJ1c2VySWQiOiIyNDEwODMiLCJleHAiOjE3MDAxMzkwNzAsImlzcyI6IkVuZXJnaW5ldCIsImp0aSI6IjY0OWUyZjUyLTgwM2MtNDNjNC05MTlmLTg4NTY5OTczZTZjYSIsInRva2VuTmFtZSI6InAxIiwiYXVkIjoiRW5lcmdpbmV0In0.5PR8JuD5ZCMUmrYwrnpaXzZvzLZlOXh0OfXtZ7wuK-o"
 
-typedef struct
-{
-    char name[MAX_LENGTH];
-    double standby_watt;
-    double running_watt;
-    int amounts;
-
-} device;
-
 // prototypes
-void fill_device(FILE *source, device *devices);
-void print_devices(device *devices);
 void get_api_fees(char answer);
 void get_api_spot_prices();
 
 int main()
 {
     FILE source;
-    device *devices;
     char answer;
     char answer1;
-
-    devices = malloc(8 * sizeof(device));
 
     printf("Do you want a new access token? y/n: ");
     scanf("%c", &answer);
@@ -48,45 +34,8 @@ int main()
 
     /* fill_device(&source, devices);
     print_devices(devices); */
-
-    free(devices);
+    
     return 0;
-}
-
-void fill_device(FILE *source, device *devices)
-{
-    source = fopen("../device.txt", "r");
-
-    if (source == NULL)
-    {
-        printf("Error the file does not exist");
-        exit(1);
-    }
-
-    char line[100];
-
-    for (int i = 0; i < 8; i++)
-    {
-        fgets(line, sizeof(line), source);
-
-        sscanf(line, "%[A-Za-z_^], %lf, %lf",
-               devices[i].name, &devices[i].standby_watt,
-               &devices[i].running_watt);
-
-        printf("Insert amounts for %s > ", devices[i].name);
-        scanf("%d", &devices[i].amounts);
-    }
-
-    fclose(source);
-}
-
-void print_devices(device *devices)
-{
-    for (int i = 0; i < 8; i++)
-    {
-        printf("%s, %.2lf, %.2lf, %d\n", devices[i].name, devices[i].standby_watt,
-               devices[i].running_watt, devices[i].amounts);
-    }
 }
 
 size_t save_api_result(char *ptr, size_t size, size_t nmemb, void *not_used)
